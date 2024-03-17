6672e81a5267aa7c2ba9cfe1718596c6dfd96c31
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-03-17 09:26:41 +0400

    font rendering

--- game/__init__.py
+++ game/__init__.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 import pygame
 from pygame import Vector2
+from pygame.freetype import Font
 
 
 def main():
+    pygame.init()
+
     # Initialize display
     win_size = Vector2(800, 600)
     win = pygame.display.set_mode(win_size)
@@ -11,6 +16,10 @@ def main():
     # Initialize clock
     fps = 60
     clock = pygame.time.Clock()
+
+    # Initialize fonts
+    font_path = Path(__file__).parent / "assets" / "fonts" / "MPLUS1Code.ttf"
+    font = Font(font_path)
 
     while True:
         dt = clock.tick(fps) / 1000
@@ -25,4 +34,14 @@ def main():
 
         # Draw
         win.fill(50)
+
+        text, text_rect = font.render("Hello, World!", "white", size=46)
+        win.blit(
+            text,
+            (
+                (win_size.x - text_rect.width) / 2,
+                (win_size.y - text_rect.height) / 2,
+            ),
+        )
+
         pygame.display.flip()
